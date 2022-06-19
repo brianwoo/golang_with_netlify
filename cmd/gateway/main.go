ccfaf5cc816a6a6613954184f9b599621fb85b7d
Brian W <woo@example.com>
2022-06-18 20:15:31 -0600

    Follow carlmjohnson's code

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -6,15 +6,15 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/apex/gateway"
 	"github.com/carlmjohnson/feed2json"
+	"github.com/carlmjohnson/gateway"
 )
 
 func main() {
 	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
 	flag.Parse()
 	listener := gateway.ListenAndServe
-	portStr := "n/a"
+	portStr := ""
 	if *port != -1 {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
@@ -22,6 +22,14 @@ func main() {
 	}
 
 	http.Handle("/api/feed", feed2json.Handler(
-		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"), nil, nil, nil))
+		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
+		nil, nil, nil, cacheControlMiddleware))
 	log.Fatal(listener(portStr, nil))
+}
+
+func cacheControlMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=300")
+		h.ServeHTTP(w, r)
+	})
 }
